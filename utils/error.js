22768d083a7c5080593ab053f3eff35b650c4f9d
S M Ghulam Ghaus Faiyaz <smghulam.ghausfaiyaz19@gmail.com>
2024-02-26 15:41:29 +0530

    removed unwanted files

--- utils/error.js
+++
@@ -1,8 +0,0 @@
-const errorHandler = (statusCode, message)=>{
-    const error = Error();
-    error.statusCode = statusCode;
-    error.message = message;
-    return error;
-};
-
-module.exports = errorHandler;
